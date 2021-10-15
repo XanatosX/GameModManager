7bce15ea6e16f54f8a3945db25bc280d31c0ca27
Simon Aberle <10531466+XanatosX@users.noreply.github.com>
2021-10-15 07:43:43 +0200

    Add missing summary blocks

--- src/GameModManager/Models/Game.cs
+++ src/GameModManager/Models/Game.cs
@@ -1,31 +1,61 @@
 ﻿using GameModManager.Services.Container;
 using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace GameModManager.Models
 {
+ 
+    /// <summary>
+    /// Class to represent a game to update it's mods
+    /// </summary>
     public class Game
     {
+        /// <summary>
+        /// Path to the exe of the game to get the image from
+        /// </summary>
         public string GameExe { get; private set; }
 
+        /// <summary>
+        /// The folder to place the downloaded files into
+        /// </summary>
         public string TargetFolderPath { get; }
 
+        /// <summary>
+        /// The remote url to get the mod from
+        /// </summary>
         public string RemoteUrl => url.Value.Url;
 
+        /// <summary>
+        /// The remote url to get the mod from
+        /// </summary>
         private readonly Lazy<UrlOpener> url;
+
+        /// <summary>
+        /// The provider to use for loading the mod and updating the client
+        /// </summary>
         public ModProvider DataProviderToUse { get; }
+
+        /// <summary>
+        /// The name of the game you are updating
+        /// </summary>
         public string Name { get; private set; }
 
+        /// <summary>
+        /// Last time the game was synced with the remote source
+        /// </summary>
         public DateTime LastUpdateTime { get; private set; }
 
+        /// <summary>
+        /// The current locally installed version
+        /// </summary>
         public Version LocalVersion { get; private set; }
 
+        /// <summary>
+        /// The current remote installed version
+        /// </summary>
         public Version RemoteVersion { get; private set; }
 
         public Game(string gameExe, string name, string targetFolderPath, string remoteUrl, ModProvider dataProviderToUse)
