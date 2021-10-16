fbc4ca07b806639e2cffca1f5f105ab3cc663574
Simon Aberle <10531466+XanatosX@users.noreply.github.com>
2021-10-16 20:35:20 +0200

    Change class to record

--- src/GameModManager/Services/Container/ReleaseArtifact.cs
+++ src/GameModManager/Services/Container/ReleaseArtifact.cs
@@ -2,19 +2,5 @@
 
 namespace GameModManager.Services.Container
 {
-    public class ReleaseArtifact
-    {
-        public Version Version { get; }
-
-        public string ArtifactFile { get; }
-
-        public string Checksum { get; }
-
-        public ReleaseArtifact(Version version, string artifactFile, string checksum)
-        {
-            Version = version;
-            ArtifactFile = artifactFile;
-            Checksum = checksum;
-        }
-    }
+    public record ReleaseArtifact(Version Version, string ArtifactFile, string Checksum);
 }
